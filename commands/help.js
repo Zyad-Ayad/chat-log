4cf057113fcd876048d9c50515381d9064c8395b
ZezoTheMaster <36793243+ZyadFVA@users.noreply.github.com>
2021-08-29 11:24:21 +0200

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,56 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+module.exports.run = async (client, message, args) => {
+
+        var list = "";
+
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./${file}`);
+
+            if (!list && command.help.help != "skip") {
+
+                var list = `\`\`${command.help.name}\`\`  :  ${command.help.help}\nUsage : ${command.help.usage}`
+
+            } else if (command.help.help != "skip"){
+
+                var list = list + `\n\n\`\`${command.help.name}\`\`  :  ${command.help.help}\nUsage : ${command.help.usage}`
+
+            }
+
+}
+
+
+
+
+
+
+
+		const commands = new Discord.MessageEmbed()
+
+		.setColor('#0099ff')
+		.setTitle("Formova commands | prefix is `.`")
+		.addField("Commands list", list)
+		.addField("NOTE :", "`[]` is required & `{}` is optinal")
+		.setFooter('Formova', 'https://g.top4top.io/p_14877vn8y1.jpg');
+		
+		message.channel.send({ embeds: [commands] })
+	
+
+
+	
+    
+
+
+
+}
+
+module.exports.help = {
+	name: "help",
+	help: "Bot commands",
+	usage: ".help"
+}
