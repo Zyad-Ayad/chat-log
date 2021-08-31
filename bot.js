3b6d50234b9f9ee4424f28db5209b2716126f0ab
ZezoTheMaster <36793243+ZyadFVA@users.noreply.github.com>
2021-08-31 17:33:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,50 @@ const Discord = require("discord.js");
 client.on("guildCreate", async guild => {
 
 let owner = await guild.fetchOwner().then(owner => owner.user)
-owner.send("Hello **" + owner.username + "**, \n\nYou or someone else just added me to **" + guild.name + "** \n\nto set up logs channel do ``.setup`` in your server!\n\nyou can manually create log channel but please make sure that channel name is ``chat-log``")
+await owner.send("Hello **" + owner.username + "**, \n\nYou or someone else just added me to **" + guild.name + "** \n\nto set up logs channel do ``.setup`` in your server!\n\nyou can manually create log channel but please make sure that channel name is ``chat-log``")
+const commands = new Discord.MessageEmbed()
+
+.setColor('#0099ff')
+.setTitle("Chat-log links")
+.addField("Bot invite link", "[Invite](https://discord.com/oauth2/authorize?client_id=879273611603619881&permissions=75792&scope=bot)", true)
+.addField("Support server", "[Formova team](https://discord.com/invite/ArfZWMhcqD)", true)
+.addField("Vote", "[Vote](https://top.gg/bot/879273611603619881/vote)", true)
+.setFooter('chat-log', 'https://i.imgur.com/2GB0fgf.png');
+
+
+
+
+const row = new MessageActionRow()
+.addComponents(
+    new MessageButton()
+        .setLabel('Invite Bot')
+        .setURL("https://discord.com/oauth2/authorize?client_id=879273611603619881&permissions=75792&scope=bot")
+        .setStyle('LINK'),
+)
+.addComponents(
+    new MessageButton()
+        .setLabel('Support server')
+        .setURL("https://discord.com/invite/ArfZWMhcqD")
+        .setStyle('LINK'),
+)
+.addComponents(
+    new MessageButton()
+        .setLabel('Vote')
+        .setURL("https://top.gg/bot/879273611603619881/vote")
+        .setStyle('LINK'),
+)
+
+
+await owner.send({ content: "Please contact us whenever you find a problem with chat-log\nor whenever you want to make suggestion", embeds: [commands], components: [row] });
+
+fetch("https://top.gg/api/bots/606872086601793557/stats", {
+    method: "POST",
+    headers: { "Content-Type": "application/json", 
+    "Authorization": process.env.topggg_token },
+    body: JSON.stringify({ "server_count": client.guilds.cache.size })
+  })
+ .then(res => res.json()) // expecting a json response
+ .then(json => console.log("Server count updated"));
 
 
 });
@@ -20,10 +63,9 @@ owner.send("Hello **" + owner.username + "**, \n\nYou or someone else just added
 client.on("ready", () => {
 
 console.log("done - " + client.user.tag)
-client.user.setActivity(".setup | Watching your messages");
+client.user.setActivity(".help | Watching your messages");
 
 })
-
 
 
 client.commands = new Collection();
