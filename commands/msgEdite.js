88e61352f658f6abb71fc852311aaa18601e0478
ZezoTheMaster <36793243+ZyadFVA@users.noreply.github.com>
2021-09-02 17:47:32 +0200

    Update msgEdite.js

--- commands/msgEdite.js
+++ commands/msgEdite.js
@@ -24,7 +24,7 @@ module.exports.run = async (client, message, args) => {
     .addField("New message content", `**${message.reactions.message.content}**`)
     .addField("Date :", date, true)
     .addField("Channel", `${message.channel}`, true)
-    .setFooter('chat-log', 'https://i.imgur.com/2GB0fgf.png');
+	.setFooter('chat-log', client.user.avatarURL());
 
 
 
