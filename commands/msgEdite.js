1c394482c11d25691d54b8fe56c9effa45360761
ZezoTheMaster <36793243+ZyadFVA@users.noreply.github.com>
2021-09-05 11:19:29 +0200

    Update msgEdite.js

--- commands/msgEdite.js
+++ commands/msgEdite.js
@@ -16,15 +16,23 @@ module.exports.run = async (client, oldMessage, newMessage) => {
     let date = now.toUTCString();
 
 
+var oldmsg = "This message has more than 1024 letters!";
+var newmsg = 'This message has more than 1024 letters!';
+
+
+if (oldMessage.content.length < 1023) oldmsg = `**${oldMessage.content}**`
+if (oldMessage.content.length < 1023) newmsg = `**${newMessage.content}**`
+
 
     const delEmbed = new Discord.MessageEmbed()
     .setColor('YELLOW')
     .setAuthor(oldMessage.author.username, oldMessage.author.avatarURL())
     .setTitle(":writing_hand: Message edited")
-    .addField("Old message content", `**${oldMessage.content}**`)
-    .addField("New message content", `**${newMessage.content}**`)
+    .addField("Old message content", `**${oldmsg}**`)
+    .addField("New message content", `**${newmsg}**`)
     .addField("Date :", date, true)
     .addField("Channel", `${oldMessage.channel}`, true)
+    .addField("Go to the message?", `[Redirect](https://discord.com/channels/${oldMessage.guild.id}/${oldMessage.channel.id}/${oldMessage.id})`, true)
     .setFooter('chat-log', client.user.avatarURL());
 
 
